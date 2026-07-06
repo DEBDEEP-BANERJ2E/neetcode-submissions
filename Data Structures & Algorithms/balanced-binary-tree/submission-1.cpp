e3886ca9220f135b9366bc3e8ad6418056121610
Debdeep Banerjee <90265059+DEBDEEP-BANERJ2E@users.noreply.github.com>
2026-07-06 12:45:32 +0530

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return dfsHeight(root) != -1;
+    }
+
+private:
+    int dfsHeight(TreeNode* node) {
+        if (!node) return 0;
+        int leftHeight = dfsHeight(node->left);
+        if (leftHeight == -1) return -1;
+        
+        // Check right subtree
+        int rightHeight = dfsHeight(node->right);
+        if (rightHeight == -1) return -1; 
+        // Right subtree is unbalanced
+        
+        // If the current node is unbalanced, return -1
+        if (abs(leftHeight - rightHeight) > 1)
+            return -1;
+        
+        // Otherwise, return the actual height of the subtree
+        return 1 + max(leftHeight, rightHeight);
+    }
+};
