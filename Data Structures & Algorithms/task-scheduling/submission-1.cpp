7d8436772439b907a929a6f2d04e1b6164d4ec74
Debdeep Banerjee <90265059+DEBDEEP-BANERJ2E@users.noreply.github.com>
2026-07-11 19:39:45 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> counts(26, 0);
+        for (char t : tasks) {
+            counts[t - 'A']++;
+        }
+        priority_queue<int> maxHeap;
+        for (int count : counts) {
+            if (count > 0)
+                maxHeap.push(count);
+        }
+        queue<pair<int, int>> q;
+        int time = 0;
+        while (!maxHeap.empty() || !q.empty()) {
+            time++;
+            if (!maxHeap.empty()) {
+                int count = maxHeap.top() - 1;
+                maxHeap.pop();
+                if (count > 0)
+                    q.push({count, time + n});
+            }
+            if (!q.empty() && q.front().second == time) {
+                maxHeap.push(q.front().first);
+                q.pop();
+            }
+        }
+        return time;
+    }
+};
