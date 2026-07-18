e1b912f3da4e1eb7a53aadfc1dd941400aa98990
Debdeep Banerjee <90265059+DEBDEEP-BANERJ2E@users.noreply.github.com>
2026-07-18 11:36:57 +0530

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> res;
+        unordered_map<int, int> map;
+        for(int i : nums){
+            map[i]++;
+        }
+        priority_queue<pair<int, int>> maxHeap;
+        for(auto& pair : map){
+            maxHeap.push({pair.second, pair.first});
+        }
+
+        for(int i=0; i<k; i++){
+            res.push_back(maxHeap.top().second);
+            maxHeap.pop();
+        }
+        return res;
+    }
+};
